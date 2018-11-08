2926314417b219a0caaf28ea878c760157036ae0
Yuanyuan Cen <yuanyuancen@CL20029.local>
2018-11-08 17:27:54 +0800

    update email address

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -10,7 +10,7 @@ setup(
     name = 'navmplot',
     version = __version__,
     author = 'Yuanyuan Cen',
-    author_email = 'pac.tric@gmail.com',
+    author_email = 'yycen@live.com',
     url = 'https://github.com/',
     description = 'Provide a matplotlib like interface to plotting data with Naver Maps',
     long_description=read('README.rst'),
