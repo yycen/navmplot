5d3c749ca35eecda8455fbd5c1db529a1bb115b1
Yuanyuan Cen <yuanyuancen@CL20029.local>
2018-11-08 17:38:35 +0800

    add url

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -11,8 +11,8 @@ setup(
     version = __version__,
     author = 'Yuanyuan Cen',
     author_email = 'yycen@live.com',
-    url = 'https://github.com/',
-    description = 'Provide a matplotlib like interface to plotting data with Naver Maps',
+    url = 'https://github.com/yycen/navmplot',
+    description = 'Provide a interface to plotting data with Naver Maps with Python',
     long_description=read('README.rst'),
     license='MIT',
     keywords='python wrapper naver maps',
